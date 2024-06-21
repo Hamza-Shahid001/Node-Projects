fb9f20b663ab6d92651d884383df421e0a754529
Muhammad hamza Shahid <hamzaunique001@gmail.com>
2024-06-21 22:59:42 +0500

    Project 6

---
+++ Word Counter/index.ts
@@ -0,0 +1,22 @@
+#! /usr/bin/env node
+
+
+import inquirer from "inquirer"
+import chalk from "chalk"
+
+console.log(chalk.yellow.bold.italic("\t\t\tWelcome to Word Counter made by Hamza")); // Display a colourful Welcome message
+console.log("\t\t"+"=".repeat(50));
+
+// Prompt to enter a sentences
+let answer =await inquirer.prompt([
+    {
+        name:"sentence",
+        type:"input",
+        message: chalk.blue("Enter a sentence:")
+    }
+])
+
+// Analysis of User Input
+let words = answer.sentence.split(" ") // Words will be separated by spaces
+
+console.log(`Words Count = ${words.length}`); // Number of words will be detected by using this line
---
+++ Word Counter/readme.md
@@ -0,0 +1,7 @@
+
+
+Just install the npx command in your text editor
+
+It will make you enter a sentence which words you want to count.
+
+Press Enter the word count will be displayed.
